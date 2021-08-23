e1b8340b9553f67971a701ec07e226678cee202c
Wander <alicia.anne.baum@gmail.com>
2021-08-22 23:32:51 -0700

    completed rough whackamole loop

--- Assets/Scripts/QuackAMole/CursorChanger.cs
+++ Assets/Scripts/QuackAMole/CursorChanger.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class CursorChanger : MonoBehaviour
 {
-    public Texture2D hammerUp;
-    public Texture2D hammerDown;
-    public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
-
-    void OnMouseExit()
-    {
-        Cursor.SetCursor(null, Vector2.zero, cursorMode);
-    }
 
     private void Update()
     {
-
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
     }
 }
